f8f071116d63ad36770c4e4febad58061a3fcdf0
theCalculatar <alpheusm13@gmail.com>
2026-06-19 21:58:16 +0200

    fix: update CallToAction component layout and styling for improved presentation

--- components/blocks/call-to-action.tsx
+++ components/blocks/call-to-action.tsx
@@ -6,58 +6,62 @@ import { Button } from "@/components/ui/button";
 import { PageBlocksCta } from "@/tina/__generated__/types";
 import { Icon } from "../icon";
 import { Section, sectionBlockSchemaField } from "../layout/section";
-import { Mail } from "lucide-react";
 
 export const CallToAction = ({ data }: { data: PageBlocksCta }) => {
   return (
     <Section background={data.background!}>
-      <div>
-        <Mail
-          className="size-30 mx-auto mb-0 rotate-10 "
-          style={{
-            maskImage:
-              "linear-gradient(to bottom, transparent 0%, black 12%, black 100%, transparent 100%)",
-            WebkitMaskImage:
-              "linear-gradient(to bottom, transparent 0%, black 12%, black 100%, transparent 100%)",
-          }}
-        />
-      </div>
-      <div className="text-center z-50">
-        <h2
-          className="text-balance text-4xl font-semibold lg:text-5xl"
-          data-tina-field={tinaField(data, "title")}
-        >
-          {data.title}
+      <div className="flex justify-center mx-2">
+        <h2 className="text-center text-4xl font-semibold lg:text-5xl">
+          Contact me <br /> Let's create
+          <br />
+          <span className="bg-zinc-400 border -rotate-3 rounded-[calc(var(--radius-2xl)+0.125rem)] lg:rounded-[calc(var(--radius-3xl)+0.125rem)] p-0.5 block w-fit -my-3 shadow-2xl">
+            <span
+              className="flex gap-1 lg:gap-2 rounded-2xl lg:rounded-3xl bg-neutral-900 p-2 lg:p-4"
+              data-tina-field={tinaField(data, "title")}
+            >
+              {data.title!.split("\n").map((v) => (
+                <span
+                  key={v}
+                  className="rounded-2xl text-xl font-bold lg:text-3xl bg-neutral-800 px-2 p-1 lg:px-3 lg:p-2 text-white"
+                >
+                  {v}
+                </span>
+              ))}
+            </span>
+          </span>
+          something great <br /> together
+          <span className="text-red-400">.</span>
         </h2>
+      </div>
+      <div className="mt-4 flex justify-center">
         <p
-          className="mt-4 capitalize"
+          className="capitalize"
           data-tina-field={tinaField(data, "description")}
         >
           {data.description}
         </p>
-
-        <div className="mt-12 flex flex-wrap justify-center gap-4">
-          {data.actions &&
-            data.actions.map((action) => (
-              <div
-                key={action!.label}
-                data-tina-field={tinaField(action)}
-                className="bg-foreground/10 rounded-[calc(var(--radius-xl)+0.125rem)] border p-0.5"
+      </div>
+      <div className="mt-4 flex flex-wrap justify-center gap-4">
+        {data.actions &&
+          data.actions.map((action) => (
+            <div
+              key={action!.label}
+              data-tina-field={tinaField(action)}
+              className="bg-foreground/10 rounded-[calc(var(--radius-xl)+0.125rem)] border p-0.5"
+            >
+              <Button
+                asChild
+                size="lg"
+                variant={action!.type === "link" ? "ghost" : "default"}
+                className="rounded-xl px-5 text-base"
               >
-                <Button
-                  asChild
-                  size="lg"
-                  variant={action!.type === "link" ? "ghost" : "default"}
-                  className="rounded-xl px-5 text-base"
-                >
-                  <Link href={action!.link!}>
-                    {action?.icon && <Icon data={action?.icon} />}
-                    <span className="text-nowrap">{action!.label}</span>
-                  </Link>
-                </Button>
-              </div>
-            ))}
-        </div>
+                <Link href={action!.link!}>
+                  {action?.icon && <Icon data={action?.icon} />}
+                  <span className="text-nowrap">{action!.label}</span>
+                </Link>
+              </Button>
+            </div>
+          ))}
       </div>
     </Section>
   );
